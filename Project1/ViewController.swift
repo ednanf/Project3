51eab532dc370d2ded2b030650d97e9f0f1274b6
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-13 09:46:04 -0300

    Part 3 completed.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 // MARK: - Properties
     var pictures: [String] = []
     
@@ -20,21 +20,26 @@ class ViewController: UIViewController {
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
+        // This loop appends the files in the filesystem that have the prefix "nssl" to the array declared above called "pictures."
         for item in items {
             if item.hasPrefix("nssl") {
                 // this is a picture to load!
                 pictures.append(item)
             }
         }
-        print(pictures)
     }
-// MARK: - Functions
-
     
+    // This function will create as many cells as needed for pictures.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
     
-    
-    
-    
+    // This function recycles the cells while scrolling, obtaining the file name and adding it as the cell's content.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath) // This method called on tableView returns a UITableViewCell
+        cell.textLabel?.text = pictures[indexPath.row] // Access the properties from the constant cell created above - the text.
+        return cell // Returns the declared cell to the function.
+    }
     
 }
 
