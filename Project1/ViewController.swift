02b5611b46866d5ba87f2e07a2fae3b00cae581b
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-13 09:07:38 -0300

    Part 2 complete.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK: - Properties
+    var pictures: [String] = []
+    
+    
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
+// MARK: - Functions
 
-
+    
+    
+    
+    
+    
+    
 }
 
